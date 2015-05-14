bfd07073f19cf7ec72d6446191dc55453ef6a424
aslesha07 <apansare@factset.com>
2015-05-14 15:55:23 -0400

    color picker added

--- client/app/create/design/design.controller.js
+++ client/app/create/design/design.controller.js
@@ -3,13 +3,9 @@
 angular.module('cakepopsApp')
   .controller('DesignCtrl', function ($scope) {
   	$scope.myColor = '';
-  	$scope.dynamicPopover = {
+  	$scope.dynamicPopoverText = {
     templateUrl: 'app/create/design/myPopoverTemplate.html',
     title: 'Enter your Text!'
   };
-
-  	$scope.btnPressed = function() {
-  		console.log('hi');
-  	}
 
   });
