01aad4fa32e52ee6624287ab0df81b4bee9cca34
aslesha07 <apansare@factset.com>
2015-05-14 19:47:56 -0400

    new changes

--- client/app/create/design/design.controller.js
+++ client/app/create/design/design.controller.js
@@ -2,10 +2,16 @@
 
 angular.module('cakepopsApp')
   .controller('DesignCtrl', function ($scope) {
-  	$scope.myColor = '';
+  	$scope.myColor = 'hsl(0, 0%, 100%)';
+    $scope.text = '';
   	$scope.dynamicPopoverText = {
-    templateUrl: 'app/create/design/myPopoverTemplate.html',
+    templateUrl: 'app/create/design/PopoverTemplateText.html',
     title: 'Enter your Text!'
-  };
-
+    };
+    $scope.dynamicPopoverShapes = {
+    templateUrl: 'app/create/design/PopoverTemplateShapes.html',
+    };
+    $scope.dynamicPopoverSprinklers = {
+    templateUrl: 'app/create/design/PopoverTemplateSprinklers.html',   
+    }
   });
