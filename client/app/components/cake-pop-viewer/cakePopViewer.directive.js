ef10e4ffccc517ba4b753fabc666aff6ea84eac6
James Ryan <jamryan@factset.com>
2015-05-14 15:34:38 -0400

    feat(CakePopViewer): Add Simple Cake Pop Display
    Added Simple Cake pop display, with stick and pop, in environmeent with
    floor and shadows.

--- client/app/components/cake-pop-viewer/cakePopViewer.directive.js
+++ client/app/components/cake-pop-viewer/cakePopViewer.directive.js
@@ -32,27 +32,19 @@
          * Initialize Three.js Viewport for Cake Pop Viewer
          */
         function init() {
-          var scene = new threejs.Scene(),
-              camera = new threejs.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000),
-              renderer = new threejs.WebGLRenderer(),
-              viewportContainer = getViewportContainer(),
-              pop;
+          var renderer = getRenderer(),
+              scene = prepareScene(),
+              camera = addCamera(scene),
+              viewPortContainer = getViewportContainer();
 
-          if (!viewportContainer) {
-            throw new Error('CakePopViewer: Could not find viewport container. 3D rendering is disabled.');
+          if (!viewPortContainer) {
+            throw new Error("CakePopViewer: Could not find viewport container. Cannot begin rendering.");
           }
 
-          renderer.setSize( window.innerWidth, window.innerHeight );
-
-          viewportContainer.appendChild( renderer.domElement );
-          pop = drawPop(scene);
-          camera.position.z = 5;
+          viewPortContainer.appendChild(renderer.domElement);
 
           var render = function() {
             requestAnimationFrame( render );
-
-            pop.rotation.x += 0.1;
-            pop.rotation.y += 0.1;
 
             renderer.render( scene, camera );
           };
@@ -61,23 +53,133 @@
         }
 
         /*
+         * Get Initialized Three.js Renderer
+         * @function
+         * @returns {object}} - Three.js renderer
+         */
+        function getRenderer() {
+          var renderer = new threejs.WebGLRenderer({ antialias: true });
+
+          renderer.setSize( window.innerWidth, window.innerHeight );
+          renderer.setClearColor(0xffffff, 1);
+          renderer.shadowMapEnabled = true;
+          renderer.shadowMapSoft = true;
+          renderer.shadowMapType = threejs.PCFSoftShadowMap;
+          renderer.physicallyBasedShading = true;
+
+          return renderer;
+        }
+
+        /*
+         * Create New Scene
+         * @function
+         * @returns {object} - New Three.js Scene
+         */
+        function prepareScene() {
+          var scene = new threejs.Scene;
+
+          // Set up Environment
+          addLighting(scene);
+          addFloor(scene);
+          addPop(scene);
+
+          return scene;
+        }
+
+        /*
+         * Add Lighting to Scene
+         * @function
+         * @param {object} scene - Three.js scene to add lighting to
+         */
+        function addLighting(scene) {
+          // Initialize Light
+          var light = new threejs.DirectionalLight( 0xFFFFFF );
+          light.position.set( 30, 80, 30 );
+          light.target.position.copy( scene.position );
+          light.castShadow = true;
+
+          // Add Light to Scene
+          scene.add( light );
+
+        }
+
+        /*
+         * Add Camera to Scene
+         * @function
+         * @param {object} scene - Scene to add camera to
+         * @returns {object} - Camera that was added to scene
+         */
+        function addCamera(scene) {
+          var camera = new threejs.PerspectiveCamera(
+            65, window.innerWidth / window.innerHeight, 1, 1000
+          );
+          camera.position.set( 50, 60, 0 );
+          camera.lookAt({
+            x: scene.position.x,
+            y: scene.position.y + 20,
+            z: scene.position.z
+          });
+
+          scene.add( camera );
+
+          return camera;
+        }
+
+        /*
          * Draw CakePop in Scene
          * @function
          * @param {object} scene - Three.js scene to draw cake pop in
          * @returns {object} - Pop Object Added to Scene
          */
-        function drawPop(scene) {
-          var geometry = new threejs.BoxGeometry( 1, 1, 1),
-              material = new threejs.MeshBasicMaterial( { color: 0x00ff00 }),
-              cube = new threejs.Mesh( geometry, material );
+        function addPop(scene) {
+          var universalGeometry = new threejs.Geometry(),
+            pop = new threejs.Mesh(
+            new threejs.SphereGeometry( 5, 32, 32 ),
+            new threejs.MeshLambertMaterial({
+              color: 0x00ff00
+            })),
+            stick = new threejs.Mesh(
+              new threejs.CylinderGeometry( 0.5, 0.5, 20, 32, 32 ),
+              new threejs.MeshLambertMaterial({
+                color: 0xFFFFFF
+              })
+            ),
+            cakePop;
 
-          scene.add( cube );
+          pop.position.set( 0, 35, 0 );
+          pop.castShadow = true;
+          pop.receiveShadow = false;
+          stick.position.set( 0, 20, 0);
+          stick.castShadow = true;
+          stick.receiveShadow = false;
 
-          return cube;
+          scene.add( pop );
+          scene.add( stick );
+
+          return pop;
+        }
+
+        /*
+         * Create Floor Plane in Scene
+         * @function
+         * @param {object} scene - Scene to add floor plane to
+         * @return {object} - Floor Plane object added to scene
+         */
+        function addFloor(scene) {
+          var ground = new threejs.Mesh(
+            new threejs.BoxGeometry(50, 1, 60),
+            new threejs.MeshLambertMaterial({
+              color: 0xffffff
+            }));
+          ground.position.set( 0, 5, 0 );
+          ground.receiveShadow = true;
+
+          scene.add( ground );
         }
 
         init();
       }
+
     };
   };
 
