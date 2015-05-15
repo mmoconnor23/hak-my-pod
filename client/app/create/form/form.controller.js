ff561aba115a23efaedc7cd4a0064c73990e198a
unknown <ybai@ybaipc.pc.factset.com>
2015-05-15 01:43:38 -0400

    s

--- client/app/create/form/form.controller.js
+++ client/app/create/form/form.controller.js
@@ -2,14 +2,19 @@
 
 angular.module('cakepopsApp')
   .controller('FormCtrl', function ($scope) {
-  	$scope.idea = "";
-  	$scope.numCakePops = '';
-  	$scope.contact = '';
-
+  	$scope.savedSuccessfully = false;
   	$scope.submit = function() {
-
+  		if (!($scope.contact && $scope.fullname && $scope.idea && $scope.numCakePops)) {
+  			alert('Enter something');
+  		} else {
+	  		localStorage.setItem(
+		  		$scope.contact, $scope.fullname + $scope.idea + $scope.numCakePops
+		  	);
+		  	$scope.savedSuccessfully = true;
+			$scope.idea = "";
+		  	$scope.numCakePops = '';
+		  	$scope.contact = '';
+		  	$scope.fullname ='';
+  		}
   	};
-
-
-
   });
